29991f382269d4e4d3b2dabe24ad99b92f35e8b3
Bikal <bikaladhikari5@gmail.com>
2024-05-04 18:42:09 +1000

    items added to local storage using json.stringify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import { nanoid } from "nanoid";
+import Items from "./components/Items";
+import { ToastContainer, toast } from "react-toastify";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    list = JSON.parse(localStorage.getItem("list"));
+  } else {
+    list = [];
+  }
+  return list;
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -36,6 +52,7 @@ const App = () => {
   };
   return (
     <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
       <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
